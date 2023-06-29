c87a479b1ff286a07f4be1274d73c6145b487937
Horváth Gellért <gellert.horvath@novin.hu>
2023-06-29 23:19:51 +0200

    format code

--- test.lua
+++ test.lua
@@ -4,3 +4,22 @@ function test()
                 local i = num+1
         print(i)
 end
+
+
+local netId = bagName:gsub('entity:', '')
+local timer = GetGameTimer()
+while not NetworkDoesEntityExistWithNetworkId(tonumber(netId)) do
+    Wait(0)
+    if GetGameTimer() - timer > 10000 then
+        return
+    end
+end
+
+local vehicle = NetToVeh(tonumber(netId))
+local timer2 = GetGameTimer()
+while NetworkGetEntityOwner(vehicle) ~= PlayerId() do
+    Wait(0)
+    if GetGameTimer() - timer2 > 10000 then
+        return
+    end
+end
