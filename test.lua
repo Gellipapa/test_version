f77efa66ad3b25de24c59b042109df2b5cee069c
Gellipapa <gellerthorvath0@gmail.com>
2023-07-02 16:34:06 +0200

    check remove empty lines

--- test.lua
+++ test.lua
@@ -1,48 +1,80 @@
 function HUD:Toggle(state)
-    SendNUIMessage({ type = 'SHOW', value = state })
+    SendNUIMessage({type = "SHOW", value = state})
 end
 
 function HUD:SetHudColor()
-    SendNUIMessage({ type = 'SET_CONFIG_DATA', value = Config })
+    SendNUIMessage({type = "SET_CONFIG_DATA", value = Config})
 end
 
 function HUD:Start(xPlayer)
-    if not xPlayer then xPlayer = ESX.GetPlayerData() end
+    if not xPlayer then
+        xPlayer = ESX.GetPlayerData()
+    end
     self:SetHudColor()
+
     self:SlowThick()
+
     self:FastThick()
 
     if not Config.Disable.Status then
+
+
         self:StatusThread()
+
+
+
+
     end
 
     if not Config.Disable.Info then
+
+
+
         self:UpdateAccounts(xPlayer.accounts)
+
+
+
     end
 
     if Config.Disable.MinimapOnFoot then
+
+
         DisplayRadar(false)
+
+
+        
     end
 
     self:Toggle(true)
 end
 
 -- Handlers
-    -- On script start
-    AddEventHandler('onResourceStart', function(resource)
-        if GetCurrentResourceName() ~= resource then return end
+-- On script start
+AddEventHandler(
+    "onResourceStart",
+    function(resource)
+        if GetCurrentResourceName() ~= resource then
+            return
+        end
         Wait(1000)
         HUD:Start()
-    end)
+    end
+)
 
-    -- On player loaded
-    AddEventHandler('esx:playerLoaded', function(xPlayer)
+-- On player loaded
+AddEventHandler(
+    "esx:playerLoaded",
+    function(xPlayer)
         Wait(1000)
         HUD:Start(xPlayer)
-    end)
+    end
+)
 
-    -- ForceLog or Logout
-    AddEventHandler('esx:onPlayerLogout', function()
+-- ForceLog or Logout
+AddEventHandler(
+    "esx:onPlayerLogout",
+    function()
         Wait(1000)
         HUD:Toggle(false)
-    end)
+    end
+)
