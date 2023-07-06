a6e0177918ef5679fbae461018383be0475ee0b6
Gellipapa <Gellipapa@users.noreply.github.com>
2023-07-06 00:26:12 +0000

    :art:Code formatted in *.lua files

--- new.lua
+++ new.lua
@@ -7,11 +7,11 @@ CreateThread(function()
         if NetworkIsPlayerActive(PlayerId()) then
             exports.spawnmanager:setAutoSpawn(false)
 
-                        DoScreenFadeOut(0)
+            DoScreenFadeOut(0)
 
-                    Wait(500)
+            Wait(500)
 
-                            TriggerServerEvent("esx:onPlayerJoined")
+            TriggerServerEvent("esx:onPlayerJoined")
 
             break
         end
@@ -19,11 +19,7 @@ CreateThread(function()
 end)
 
 RegisterNetEvent("esx:requestModel", function(model)
-
-        
     ESX.Streaming.RequestModel(model)
-
-        
 end)
 
 RegisterNetEvent("esx:playerLoaded")
